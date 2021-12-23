bf9aafdb32186683f6b349c12e4dcd859f2bdffb
David Viloria <david.viloria0424@gmail.com>
2021-12-22 21:52:48 -0500

    Creamos una extension de UIButton

--- Proyecto Musical/Extensiones.swift
+++ Proyecto Musical/Extensiones.swift
@@ -5,4 +5,41 @@
 //  Created by David Viloria Ortega on 22/12/21.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable extension UIButton{
+    
+    @IBInspectable var borderWidth: CGFloat{
+        set {
+            layer.borderWidth = newValue
+        }
+        
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat{
+        set {
+            layer.cornerRadius = newValue
+        }
+        
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor?{
+        set {
+            guard let uiColor = newValue else {return} //Intenta crear uiColor a partir de el newValue del borderColor
+            layer.borderColor = uiColor.cgColor
+        }
+        
+        get {
+            guard let color = layer.borderColor else {return nil}
+            return UIColor(cgColor: color)
+        }
+    }
+    
+    
+}
